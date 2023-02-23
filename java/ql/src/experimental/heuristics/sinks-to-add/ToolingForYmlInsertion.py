e093981ab1c6ad1df3d3faf3591fe186c926c8e0
Jami Cogswell <jcogs33@Jamis-MacBook-Pro.local>
2023-02-23 18:14:20 -0500

    Java: more updates to yml tooling

--- java/ql/src/experimental/heuristics/sinks-to-add/ToolingForYmlInsertion.py
+++ java/ql/src/experimental/heuristics/sinks-to-add/ToolingForYmlInsertion.py
@@ -78,7 +78,6 @@
 import yaml
 import ruamel.yaml
 from ruamel.yaml.scalarstring import DoubleQuotedScalarString as dq_str
-
 import io
 import sys
 
@@ -113,50 +112,41 @@ import sys
 # #     print(i)
 
 # * with ruamel.yaml
-yaml = ruamel.yaml.YAML()
+# yaml = ruamel.yaml.YAML()
 
-# settings:
-yaml.preserve_quotes = True # to keep the double-quotes
-yaml.indent(mapping=2, sequence=4, offset=2) # indentation stays the same
-yaml.width = 4096 # yml rows stay on one line (hopefully 4096 is always long enough, can adjust if run into a case where not)
-yaml.boolean_representation = ['False', 'True'] # preserve uppercase for booleans
-yaml.default_flow_style = None # force added rows to be single-line format
-#yaml.default_style='"' # doesn't work with booleans... and seems to break default_flow_style..., and wraps too much in quotes
-# read in existing yml file
-with open("java/ql/lib/ext/org.apache.http.model.yml", "r") as f:
-    data = yaml.load(f.read())
+# # settings:
+# yaml.preserve_quotes = True # to keep the double-quotes
+# yaml.indent(mapping=2, sequence=4, offset=2) # indentation stays the same
+# yaml.width = 4096 # yml rows stay on one line (hopefully 4096 is always long enough, can adjust if run into a case where not)
+# yaml.boolean_representation = ['False', 'True'] # preserve uppercase for booleans
+# yaml.default_flow_style = None # force added rows to be single-line format
+# #yaml.default_style='"' # doesn't work with booleans... and seems to break default_flow_style..., and wraps too much in quotes
 
-# Debugging prints:
-# print(data)
-# for ext in data['extensions']:
-#     for row in ext['data']:
-#         print(row)
+# # read in existing yml file
+# with open("java/ql/lib/ext/org.apache.http.model.yml", "r") as f:
+#     data = yaml.load(f.read())
 
-# Insert new models into `data`:
-# * code['name']['given'] = 'Bob'
-# * data['abc'].append('b')
-# * data.insert(1, 'last name', 'Vandelay', comment="new key")
-# data.preserve_quotes = True
-# data.default_style='"'
-#data.fa.set_flow_style()
-print(data['extensions'][0]['data']) # this is a LIST that can `append`, `insert`, `extend`, etc.
-given_list = ["org.apache.http", "HttpHost", True, "HttpHost", "(HttpHost)", "", "Argument[0]", "%-url", "manual"]
-#given_list_csv = "[\"org.apache.http\", \"HttpHost\", True, \"HttpHost\", \"(HttpHost)\", \"\", \"Argument[0]\", \"%-url\", \"manual\"]"
-# test_list = []
-# for i in given_list:
-#     if i not in [True, False]:
-#         test_list.append(dq_str(i))
-#     else:
-#         test_list.append(i)
-# print(test_list)
-data['extensions'][0]['data'].insert(0, given_list) # insert row with and without comment
-data['extensions'][0]['data'].yaml_add_eol_comment('NEW COMMENT', 0) # add eol_comment to added row
-data['extensions'][0]['data'].sort() # seems to work for maintaining alphabetical ordering of models
+# # Insert new models into `data`:
+# # * code['name']['given'] = 'Bob'
+# # * data['abc'].append('b')
+# # * data.insert(1, 'last name', 'Vandelay', comment="new key")
+# print(data['extensions'][0]['data']) # this is a LIST that can `append`, `insert`, `extend`, etc.
+# given_list = ["org.apache.http", "HttpHost", True, "HttpHost", "(HttpHost)", "", "Argument[0]", "%-url", "manual"]
+# #given_list_csv = "[\"org.apache.http\", \"HttpHost\", True, \"HttpHost\", \"(HttpHost)\", \"\", \"Argument[0]\", \"%-url\", \"manual\"]"
+# # test_list = []
+# # for i in given_list:
+# #     if i not in [True, False]:
+# #         test_list.append(dq_str(i))
+# #     else:
+# #         test_list.append(i)
+# # print(test_list)
+# data['extensions'][0]['data'].insert(0, given_list) # insert row with and without comment
+# data['extensions'][0]['data'].yaml_add_eol_comment('NEW COMMENT', 0) # add eol_comment to added row
+# data['extensions'][0]['data'].sort() # seems to work for maintaining alphabetical ordering of models
 
-
-# write out same file with same formatting
-with open("java/ql/src/experimental/heuristics/sinks-to-add/test.yml", "w") as f2:
-    yaml.dump(data, f2) # ! can't use sort_keys=True or default_flow_style=False with this it seems
+# # write out `data` to new file with same formatting
+# with open("java/ql/src/experimental/heuristics/sinks-to-add/test.yml", "w") as f2:
+#     yaml.dump(data, f2) # ! can't use sort_keys=True or default_flow_style=False with this it seems
 
 # DONE: yml rows stay on one line: https://stackoverflow.com/questions/42170709/prevent-long-lines-getting-wrapped-in-ruamel-yaml,
 # DONE: indentation stays the same: https://yaml.readthedocs.io/en/latest/detail.html?highlight=indent#indentation-of-block-sequences
@@ -172,3 +162,115 @@ with open("java/ql/src/experimental/heuristics/sinks-to-add/test.yml", "w") as f
 # TODO: maintain double-quotes on *inserted* rows: https://stackoverflow.com/questions/39262556/preserve-quotes-and-also-add-data-with-quotes-in-ruamel, https://stackoverflow.com/questions/38784766/adding-quotes-using-ruamel-yaml
 #       - weirdly hard to do this while maintaining the `yaml.default_flow_style = None`..., maybe worth abandoning double-quotes in all yml files due to this?
 #       - commentmap might work: https://stackoverflow.com/questions/70852345/force-string-quoting-while-saving-flow-style
+
+
+
+# *** MAIN CODE ***
+from csv import DictReader
+import ruamel.yaml
+
+# yml setup
+yaml = ruamel.yaml.YAML()
+yaml.preserve_quotes = True # to keep the double-quotes
+yaml.indent(mapping=2, sequence=4, offset=2) # indentation stays the same
+yaml.width = 4096 # yml rows stay on one line (hopefully 4096 is always long enough, can adjust if run into a case where not)
+yaml.boolean_representation = ['False', 'True'] # preserve uppercase for booleans
+yaml.default_flow_style = None # force added rows to be single-line format
+
+# Read csv into List of Dicts (where each dict is a model)
+with open("java/ql/src/experimental/heuristics/sinks-to-add/TestToolingMaDHeuristics.csv", "r") as f:
+    dict_reader = DictReader(f)
+    list_of_dict = list(dict_reader)
+
+# read existing yml into data structure so can insert into it
+# TODO: check if yml file exists for package
+# TODO: move reading of yml to after get `package_name` and `yml_filename` below
+# TODO: handle insertion of `extensions`, etc. in new file if yml_file does not exist
+# below from test case generator may help:
+'''
+if len(supportModelRows) != 0:
+    # Make a test extension file
+    with open(resultYml, "w") as f:
+        models = "\n".join('      - [%s]' %
+                          modelSpecRow[0].strip() for modelSpecRow in supportModelRows)
+        dataextensions = f"""extensions:
+  - addsTo:
+      pack: codeql/java-tests
+      extensible: summaryModel
+    data:
+{models}
+"""
+        f.write(dataextensions)
+'''
+with open("java/ql/lib/ext/org.apache.http.model.yml", "r") as f:
+    yml_data = yaml.load(f.read())
+
+# get package name from model (to find appropriate yml file) - skip any models that are not "sink", "source", "step", "neutral", "sinkOrStep" - only add definite models with this script for now
+# and get corresponding yml filename
+files_modified_set = set()
+package_name = ""
+yml_filename = ""
+for model in list_of_dict:
+    # TP models insertion:
+    if model["ModelType"] in ["sink", "source", "step", "neutral", "sinkOrStep"]:
+
+        # get necessary info
+        comment = model["Notes"]
+        new_model = model["Proposed Sink"].strip("][").split(", ") # convert model string to list
+        new_model = [item.strip('"') for item in new_model] # strip quotes from strings
+        new_model[2] = eval(new_model[2]) # eval to get boolean value of string True/False
+        #print(new_model)
+        #sys.exit(0)
+
+        # get package name and yml filename
+        package_name = model["Proposed Sink"].split(",")[0][2:-1]
+        yml_filename = "java/ql/lib/ext/{}.model.yml".format(package_name) # TODO: don't hardcode path?
+        # track what files are modified, # TODO: adjust how this is done, so not continually attempting to add duplicates to set
+        #files_modified_set.add(yml_filename)
+
+        # DEBUGGING-ONLY
+        # TODO: make work for ANY yml files
+        if yml_filename == "java/ql/lib/ext/org.apache.http.model.yml":
+            files_modified_set.add(yml_filename)
+            # insert model(s) into yml data structure
+            # TODO: check for duplicates
+            # TODO: remove 'NEW MODEL' comments and only add comments `if model["Notes"] not in ["", " "]:`
+            # TODO: add comment about modeltype (esp for sinkOrStep ones)
+            # TODO: condense all `model["ModelType"]`, etc. into variables
+            # TODO: find if sink, source, step, neutral, sinkOrStep and insert in proper data['extensions'][n]['addsTo']['extensible']
+            # TODO: assuming source_loc=0, sink_loc=1, and summary_loc=2, neutral_loc=4 in yml file ordering, change below to not assume this, but to actually determine from yml_data (esp since some files might not have all)
+            # TODO: handle insertion of `extensions`, etc. if that model-type doesn't exist in the current file
+            source_loc = 0
+            sink_loc = 1
+            summary_loc = 2
+            neutral_loc = 3
+            if model["ModelType"] == "sink" or model["ModelType"] == "sinkOrStep":
+                yml_data['extensions'][sink_loc]['data'].insert(0, new_model) # insert row at top
+                yml_data['extensions'][sink_loc]['data'].yaml_add_eol_comment('NEW MODEL, ' + comment, 0) # add eol_comment to added row
+                yml_data['extensions'][sink_loc]['data'].sort() # seems to work for maintaining alphabetical ordering of models
+            elif model["ModelType"] == "source":
+                yml_data['extensions'][source_loc]['data'].insert(0, new_model) # insert row at top
+                yml_data['extensions'][source_loc]['data'].yaml_add_eol_comment('NEW MODEL, ' + comment, 0) # add eol_comment to added row
+                yml_data['extensions'][source_loc]['data'].sort() # seems to work for maintaining alphabetical ordering of models
+            elif model["ModelType"] == "step": # aka summary
+                yml_data['extensions'][summary_loc]['data'].insert(0, new_model) # insert row at top
+                yml_data['extensions'][summary_loc]['data'].yaml_add_eol_comment('NEW MODEL, ' + comment, 0) # add eol_comment to added row
+                yml_data['extensions'][summary_loc]['data'].sort() # seems to work for maintaining alphabetical ordering of models
+            # elif model["ModelType"] == "neutral":
+            #     yml_data['extensions'][neutral_loc]['data'].insert(0, new_model_quotes_removed) # insert row at top
+            #     yml_data['extensions'][neutral_loc]['data'].yaml_add_eol_comment('NEW MODEL, ' + comment, 0) # add eol_comment to added row
+            #     yml_data['extensions'][neutral_loc]['data'].sort() # seems to work for maintaining alphabetical ordering of models
+            else:
+                print("ModelType, " + model["ModelType"] + ", not correct.") # TODO: make this and all error messages better
+        else:
+            print("!!! " + model["Proposed Sink"] + " not inserted!!!")
+
+
+# write modified yml back to same file (separate file during testing)
+with open("java/ql/src/experimental/heuristics/sinks-to-add/test.yml", "w") as f2:
+    yaml.dump(yml_data, f2)
+
+# tell user what files modified, print any errors, etc.
+print("FILES MODIFIED:")
+for item in files_modified_set:
+    print("  " + item)
