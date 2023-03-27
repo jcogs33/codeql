4dcdfd63e1d0cc57fafd4d75891c6969cbceaf53
Jami Cogswell <jcogs33@Jamis-MacBook-Pro.local>
2023-03-26 20:19:54 -0400

    Java: add initial stubs for top 500 jdk apis test

---
+++ java/ql/test/query-tests/Metrics/GeneratedVsManualCoverage/TopJdkApisTest/TopJdkApiStubs/java/lang/Thread.java
@@ -0,0 +1,16 @@
+// Generated automatically from java.lang.Thread for testing purposes
+
+package java.lang;
+
+import java.util.Map;
+
+public class Thread implements Runnable
+{
+    public ClassLoader getContextClassLoader(){ return null; } // manual neutral
+    public Thread(Runnable p0){} // manual neutral
+    public final String getName(){ return null; } // manual neutral
+    public static Thread currentThread(){ return null; } // manual neutral
+    public static void sleep(long p0){} // manual neutral
+    public void interrupt(){} // manual neutral
+    public void start(){} // manual neutral
+}
